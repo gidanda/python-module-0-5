27d05ba736cf7faf7e9509117c583848cda0fcd9
hiroki goda <hiroz000qop@gmail.com>
2026-05-01 20:19:35 +0900

    04-00 done

---
+++ 04/ex0/ft_ancient_text.py
@@ -0,0 +1,27 @@
+import sys
+import typing
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: ft_ancient_text.py <file>")
+        return
+
+    filename = sys.argv[1]
+    
+    print("=== Cyber Archives Recovery ===")
+    print(f"Accessing file '{filename}'")
+
+    try:
+        file = open(filename)
+        content = file.read()
+        print("---")
+        print(content)
+        print("---")
+        file.close()
+        print(f"File '{filename}' closed.")
+
+    except Exception as error:
+        print(f"Error opening file '{filename}': {error}")
+
+if __name__ == "__main__":
+    main()
